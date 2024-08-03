6d76b1b5c7465edef711f8a2f735d933073d0450
SriramChowdaryMogalapu <mogalapusriram391@gmail.com>
2024-08-03 16:06:22 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(PROCESS.env.MONGO_URI,{});
+    const conn = await mongoose.connect(process.env.MONGO_URI,{});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(`MongoDB Error : ${err.message}`);
