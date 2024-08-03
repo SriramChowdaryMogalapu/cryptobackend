7f72ec9030f2aacdfc87e8b2c0961d380d32313e
SriramChowdaryMogalapu <mogalapusriram391@gmail.com>
2024-08-03 15:50:16 +0530

    db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://msrc:Is8UirnFX68bHEkg@crypto-notes.pae1ets.mongodb.net/?retryWrites=true&w=majority&appName=crypto-notes',{});
+    const conn = await mongoose.connect(PROCESS.env.MONGO_URI,{});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(`MongoDB Error : ${err.message}`);
